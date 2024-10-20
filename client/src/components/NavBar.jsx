80b90a08162ac096b16490b55577a94845d4cc91
Imtiaz Ahmed <imtiaz.uap@gmail.com>
2024-10-21 03:01:55 +0600

    updated home link

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,22 +1,22 @@
 // src/components/NavBar.js
-import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import { Link } from 'react-router-dom';
-import { Box } from '@mui/material';
-import { styled } from '@mui/system';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { Link } from "react-router-dom";
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
 
 const NavLink = styled(Link)(({ theme }) => ({
-  color: 'inherit',
-  textDecoration: 'none',
+  color: "inherit",
+  textDecoration: "none",
   marginRight: theme.spacing(2),
 }));
 
@@ -36,14 +36,14 @@ const NavBar = () => {
             color="inherit"
             aria-label="menu"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            Land Vista
+            <NavLink to="/">Land Vista</NavLink>
           </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+          <Box sx={{ display: { xs: "none", sm: "flex" } }}>
             <NavLink to="/">
               <Button color="inherit">Map View</Button>
             </NavLink>
@@ -58,16 +58,26 @@ const NavBar = () => {
         anchor="left"
         open={drawerOpen}
         onClose={handleDrawerToggle}
-        sx={{ display: { xs: 'block', sm: 'none' } }}
+        sx={{ display: { xs: "block", sm: "none" } }}
       >
         <List>
           <ListItem button component={Link} to="/" onClick={handleDrawerToggle}>
             <ListItemText primary="Home" />
           </ListItem>
-          <ListItem button component={Link} to="/search" onClick={handleDrawerToggle}>
+          <ListItem
+            button
+            component={Link}
+            to="/search"
+            onClick={handleDrawerToggle}
+          >
             <ListItemText primary="Search" />
           </ListItem>
-          <ListItem button component={Link} to="/create" onClick={handleDrawerToggle}>
+          <ListItem
+            button
+            component={Link}
+            to="/create"
+            onClick={handleDrawerToggle}
+          >
             <ListItemText primary="Create New" />
           </ListItem>
           <ListItem button onClick={handleDrawerToggle}>
