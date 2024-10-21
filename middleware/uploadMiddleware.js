6317ddaa4d233d65797633dd2ccccb5c976c2e1f
Imtiaz Ahmed <imtiaz.uap@gmail.com>
2024-10-22 02:56:10 +0600

    reverting directory changes

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,19 +1,12 @@
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
-
-// Use the temporary directory for serverless environments
-const uploadDir = '/tmp/uploads/';
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);  // Save files to the '/tmp/uploads/' directory
+    cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));  // Use timestamp as filename
+    cb(null, Date.now() + path.extname(file.originalname));
   }
 });
 
