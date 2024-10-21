e58f6055774d8603d0e838931da7f4ebc294360f
Imtiaz Ahmed <imtiaz.uap@gmail.com>
2024-10-22 02:47:35 +0600

    creating uploads diretory if not available

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,12 +1,19 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+// Ensure the uploads directory exists
+const uploadDir = 'uploads/';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, uploadDir);  // Save files to 'uploads/' directory
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + path.extname(file.originalname));  // Use timestamp as filename
   }
 });
 
