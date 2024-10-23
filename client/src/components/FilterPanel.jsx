a3e7409ffefed790526a6d235d04daf04ef19203
Imtiaz Ahmed <imtiaz.uap@gmail.com>
2024-10-23 23:13:06 +0600

    updated filter options

--- client/src/components/FilterPanel.jsx
+++ client/src/components/FilterPanel.jsx
@@ -92,9 +92,12 @@ const FilterPanel = ({ filter, setFilter, onApplyFilter, onClearFilter }) => {
               renderValue={(selected) => selected.join(", ")}
             >
               <MenuItem value="For Sale">For Sale</MenuItem>
-              <MenuItem value="For Rent">For Rent</MenuItem>
               <MenuItem value="Sold">Sold</MenuItem>
+              <MenuItem value="For Rent">For Rent</MenuItem>
+              <MenuItem value="Rented">Rented</MenuItem>
+              <MenuItem value="For Lease">For Lease</MenuItem>
               <MenuItem value="Leased">Leased</MenuItem>
+              <MenuItem value="Not Available">Not Available</MenuItem>
               <MenuItem value="Auction">Auction</MenuItem>
             </Select>
           </FormControl>
